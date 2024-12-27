1fb66af625bb80a142113a726c9142c24cb0ef13
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-27 17:08:40 +0200

    Main component App (roter components)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import { USER_INFO } from "./constans.ts";
+import { UserInfo } from "./types.ts";
+import { storage } from "./utils.ts";
+import WelcomeScreen from "./components/WelcomeScreen.tsx";
+import ChatScreen from "./components/ChatScreen.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userInfo = storage.get<UserInfo>(USER_INFO);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="w-[480px] h-full mx-auto flex flex-col py-4">
+      {userInfo ? <ChatScreen /> : <WelcomeScreen />}
+    </section>
+  );
 }
 
-export default App
+export default App;
