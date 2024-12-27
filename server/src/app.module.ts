c65c38ff68ff0acca35dc89e8713ed7f9188f34c
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-27 15:54:44 +0200

    implementation websocket gateway

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, AppGateway],
 })
 export class AppModule {}
