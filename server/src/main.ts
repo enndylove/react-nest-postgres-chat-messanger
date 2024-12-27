a9873c7fa65ef29cd630879ef467cc0cd7ff8c29
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-27 17:12:52 +0200

    enable CORS for CLIENT_URI variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { PrismaService } from "./prisma.service";
+import { CLIENT_URI } from "../constants";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: CLIENT_URI,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Range", "X-Total-Count"],
+  });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3001);
